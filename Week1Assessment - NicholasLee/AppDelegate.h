d0bdea0ce21385ab0ada4e36dbff124576d25342
nicholaslee <nicholaslee@nicholaslees-MacBook-Pro.local>
2017-03-20 10:48:57 +0800

    Initial Commit

---
+++ Week1Assessment - NicholasLee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Week1Assessment - NicholasLee
+//
+//  Created by nicholaslee on 20/03/2017.
+//  Copyright © 2017 nicholaslee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Week1Assessment - NicholasLee/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Week1Assessment - NicholasLee
+//
+//  Created by nicholaslee on 20/03/2017.
+//  Copyright © 2017 nicholaslee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
